23e1d17a883dfcd5e734c26ce702ab4bcba082bc
Guillaume Riflet <guillaume.riflet@celfocus.com>
2018-08-29 23:30:54 +0100

    new maven project hello world

---
+++ mvn-release-plugin-helloworld/helloworld/src/main/java/org/riflet/guillaume/mvn/plugin/release/App.java
@@ -0,0 +1,13 @@
+package org.riflet.guillaume.mvn.plugin.release;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
